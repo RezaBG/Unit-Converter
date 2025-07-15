9d5a5e3b3cafd5423d695668f4cb069f25a0741b
RezaBG <101470327+RezaBG@users.noreply.github.com>
2025-07-15 16:47:32 +0200

    feat: add initial implementation of the unit converter server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+
+	fmt.Println("Server starting on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the home page!")
+}
